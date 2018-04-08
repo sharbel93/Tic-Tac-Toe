2e7c5baeaa55e22ed3d8be2a148798868680f1c5
sharbel93 <sharbel.chris@gmail.com>
2018-04-08 04:54:11 +0300

    one player activity

---
+++ app/src/main/java/com/example/geek_sharbel/tictactoe/PlayerWithComputerActivity.java
@@ -0,0 +1,13 @@
+package com.example.geek_sharbel.tictactoe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlayerWithComputerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player_with_computer);
+    }
+}
